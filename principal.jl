7127f01645f57e513b9681c77a9224ed27ec3bf8
J. L. Girvent <girvent.27@gmail.com>
2024-04-03 19:35:30 -0300

    kkkkk

---
+++ principal.jl
@@ -0,0 +1,5 @@
+function menu(title::String , options::Array)
+    println("============================================================= $title =============================================================")
+    println("Opcões: $options")
+    println("Digite uma opcão:")
+end
