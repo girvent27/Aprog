6dd70056108189ea7edd5b09c77148aca9ee2d40
J. L. Girvent <girvent.27@gmail.com>
2024-04-07 23:32:07 -0300

    Algumas Mudanças 23-32

--- principal.jl
+++ principal.jl
@@ -1,15 +1,31 @@
+function main()
+    pages = [1, 0, 0]
+
+    while pages[1] != 0 
+        menu("+  Conversor de Medidas, Unidades e Moedas  +", pages)
+        print("Digite uma opção:")
+        pages[1] = parse(Int, readline())
+        clear()
+
+    end
+end
+
+
+
 function menu(title::String , options)
-    println("============================================================= $title =============================================================")
+    println("==== $title ====")
     println("Opcões:")
     #foreach(x -> println(x), options)
-    for i in options
+    for i in 1:length(options)
         print("$i - ")
         println(options[i])
     end
-    print("Digite uma opcão:")
-    parse(Int, readline())
-
-    return "oi"
 end
+
+
+function clear()
+    Base.run(`dir`)
+end
+
 
  
